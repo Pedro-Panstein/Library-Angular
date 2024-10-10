d3dbae28689887990eccca844d5ede7dce7c94fe
pedro <phfarof@gmail.com>
2024-10-09 23:44:22 -0300

    🛠️ Finalized Login Page Features
    Input validation and change the send button, both its style and the send function by pressing enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ButtonComponent } from './components/button/button.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ButtonComponent
+    ButtonComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   userName: string = '';
@@ -12,13 +12,29 @@ export class LoginComponent {
   constructor(private rota: Router) {}
 
   login() {
-    if (this.userName === undefined) {
-      alert("Digite um nome")
+    const regex = /[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~°ªº§¢£¬¨´´®¥©×÷±]/;
+
+    if (this.userName == null || this.userName == '') {
+      this.error();
+    } else if (regex.test(this.userName)) {
+      this.error();
     } else if (this.userName.length > 35) {
-      alert("O limite de caracteres é 35")
+      this.error();
     } else {
       sessionStorage.setItem('user', this.userName);
-      this.rota.navigate(['home'])
+      this.rota.navigate(['home']);
     }
+  }
+
+  error() {
+    const errorMessage = document.querySelector('.error-message');
+    const input = document.querySelector('#input-username');
+
+    errorMessage?.classList.remove('hidden');
+    input?.classList.add('input-error');
+    setTimeout(() => {
+      input?.classList.remove('input-error');
+      errorMessage?.classList.add('hidden');
+    }, 3000);
   }
 }
