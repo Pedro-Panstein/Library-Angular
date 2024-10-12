84fb86c8dbadbca50cc8d34cfc4050f3ac89e504
Pedro Henrique Panstein <phfarof@gmail.com>
2024-10-11 21:46:52 -0300

    🛠️ Feature of adding an image created and configured
    Drag and drop created for file input, also defined to accept only .png and .jpeg

--- src/app/pages/modals/modal-add-book/modal-add-book.component.ts
+++ src/app/pages/modals/modal-add-book/modal-add-book.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-add-book',
   templateUrl: './modal-add-book.component.html',
-  styleUrl: './modal-add-book.component.scss'
+  styleUrls: ['./modal-add-book.component.scss']
 })
 export class ModalAddBookComponent {
-  
+  constructor(public dialogRef: MatDialogRef<ModalAddBookComponent>) {}
+
+  uploadFile() {
+    const inputFile = document.getElementById('file') as HTMLElement;
+    inputFile?.click();
+  }
+
+  onFileSelected(event: any) {
+    const textUpload = document.querySelector('.drag-select p') as HTMLElement;
+    const file = event.target.files[0];
+    this.handleFileUpload(file, textUpload);
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    const textUpload = document.querySelector('.drag-select p') as HTMLElement;
+    const file = event.dataTransfer?.files[0];
+    if (file) {
+      this.handleFileUpload(file, textUpload);
+    }
+    this.resetDragStyles();
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    const uploadArea = event.target as HTMLElement;
+    uploadArea.classList.add('dragging');
+  }
+
+  onDragLeave(event: DragEvent) {
+    this.resetDragStyles();
+  }
+
+  handleFileUpload(file: File, textUpload: HTMLElement) {
+    if (!file) return;
+
+    const validTypes = ['image/png', 'image/jpeg'];
+    if (!validTypes.includes(file.type)) {
+      textUpload.textContent = 'Arquivo inválido';
+      textUpload.style.color = 'var(--red)';
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      textUpload.style.color = 'var(--gray)';
+      const imageElement = document.getElementById('image') as HTMLImageElement;
+      imageElement.src = e.target.result;
+      imageElement.style.marginRight = '15px';
+    };
+
+    reader.readAsDataURL(file);
+    textUpload.textContent = 'Clique aqui caso queira selecionar outra imagem';
+  }
+
+  resetDragStyles() {
+    const uploadArea = document.querySelector('.upload-image') as HTMLElement;
+    uploadArea.classList.remove('dragging');
+  }
+
+  closeModal() {
+    this.dialogRef.close();
+  }
 }
